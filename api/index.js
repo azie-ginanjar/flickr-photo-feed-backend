e24d2c87a32af7dd3a2aed1a15d1c0b822bd619b
azie-ginanjar <xstepnort@gmail.com>
2020-08-23 05:55:53 +0700

    change port for test to run on port 8080, and setting NODE_ENV to test on test

--- api/index.js
+++ api/index.js
@@ -1,11 +1,17 @@
 import express from 'express';
 import bodyParser from 'body-parser';
-import flickrRoutes from './server/routes/FlickrRoutes'
+import flickrRoutes from './server/routes/FlickrRoutes';
 
 const app = express();
+const env = process.env.NODE_ENV || 'development';
+
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: false}));
-const port = process.env.PORT || 8000;
+
+let port = process.env.PORT || 8000;
+if (env === 'test'){
+  port = 8080;
+}
 
 app.use('/api/v1/photo', flickrRoutes);
 
